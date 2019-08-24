6fa9ca89a888d3453ce970b85bceb9df51c2dbbf
Maytham Fahmi <maythamfahmi@itbackyard.com>
2019-08-24 22:54:36 +0200

    Init project - ImageUtils with base method.

---
+++ ImgConverter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using ImageMagick;
+
+namespace ImgConverter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var src = @"C:\temp\Images\";
+            var dest = @"C:\temp\Output";
+
+            var iu = new ImageUtils(src);
+            iu.ConvertToImage(dest);
+        }
+        
+    }
+}
